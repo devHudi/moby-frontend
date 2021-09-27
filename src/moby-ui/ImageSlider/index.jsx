c1d033465dd875f76fd361ab4d37b625575a4098
devHudi <devhudi@gmail.com>
2021-09-27 22:34:29 +0900

    feat: implement ImageSlider

---
+++ src/moby-ui/ImageSlider/index.jsx
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Slider from 'react-slick';
+import _ from 'lodash';
+
+const settings = {
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
+const Wrapper = styled.div`
+  & .slick-dots {
+    bottom: 15px !important;
+  }
+
+  & .slick-dots li button:before {
+    color: #ffffff;
+  }
+`;
+
+const Image = styled.div`
+  height: 295px;
+  background-image: url(${(props) => props.image});
+  background-size: cover;
+  background-position: center;
+`;
+
+const ImageSlider = ({ images }) => (
+  <Wrapper>
+    <Slider {...settings}>
+      {_.map(images, (image) => (
+        <Image image={image} />
+      ))}
+    </Slider>
+  </Wrapper>
+);
+
+ImageSlider.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.string),
+};
+
+ImageSlider.defaultProps = {
+  images: [],
+};
+
+export default ImageSlider;
