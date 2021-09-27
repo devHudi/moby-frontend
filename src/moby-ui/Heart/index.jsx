8784a65c930b88d6d5d85da9f9e17f27314669da
devHudi <devhudi@gmail.com>
2021-09-27 22:56:39 +0900

    feat: implement Heart

---
+++ src/moby-ui/Heart/index.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+
+const Wrapper = styled.div`
+  display: inline-block;
+  color: ${(props) => (props.filled ? '#FF381A' : props.borderColor)};
+  font-size: ${(props) => props.size}px;
+`;
+
+const Heart = ({ defaultFilled, borderColor, size, onChange }) => {
+  const [filled, setFilled] = useState(defaultFilled);
+
+  const onClick = () => {
+    if (onChange) {
+      onChange(!filled);
+      setFilled(!filled);
+    }
+  };
+
+  return (
+    <Wrapper
+      filled={filled}
+      borderColor={borderColor}
+      size={size}
+      onClick={onClick}
+    >
+      {filled ? <AiFillHeart /> : <AiOutlineHeart />}
+    </Wrapper>
+  );
+};
+
+Heart.propTypes = {
+  defaultFilled: PropTypes.bool,
+  borderColor: PropTypes.string,
+  size: PropTypes.number,
+  onChange: PropTypes.func,
+};
+
+Heart.defaultProps = {
+  defaultFilled: false,
+  borderColor: '#ffffff',
+  size: 18,
+  onChange: null,
+};
+
+export default Heart;
