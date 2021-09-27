9efbac36f421c71f5e61e254f7e575f5cc5f9521
devHudi <devhudi@gmail.com>
2021-09-27 22:09:52 +0900

    feat: implement Header

---
+++ src/moby-ui/Header/index.jsx
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import { Flex } from '..';
+
+import logoIcon from './logo-icon.png';
+
+const Wrapper = styled.div`
+  padding: 55px 17px 0 17px;
+  background-color: #ffffff;
+`;
+
+const Title = styled.div`
+  font-size: 22px;
+  font-weight: bold;
+`;
+
+const Children = styled.div`
+  padding-bottom: 10px;
+  background-color: #ffffff;
+`;
+
+const LogoIcon = styled.img`
+  margin-left: 15px;
+`;
+
+const Header = ({ title, children }) => (
+  <Wrapper>
+    <Flex align="center" direction="row">
+      <Title>{title}</Title>
+      <LogoIcon src={logoIcon} />
+    </Flex>
+    {children && <Children>{children}</Children>}
+  </Wrapper>
+);
+
+Header.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
+
+Header.defaultProps = {
+  title: '',
+  children: null,
+};
+
+export default Header;
