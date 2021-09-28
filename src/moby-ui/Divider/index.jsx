dc90b3eb158e1d5c41c68479bb328151ef9e0ee4
devHudi <devhudi@gmail.com>
2021-09-29 00:58:25 +0900

    feat: add color prop

--- src/moby-ui/Divider/index.jsx
+++ src/moby-ui/Divider/index.jsx
@@ -3,28 +3,30 @@ import styled from 'styled-components';
 
 const HLine = styled.div`
   width: ${(props) => (props.size === null ? '100%' : `${props.size}px`)};
-  border-bottom: 1px solid #cecece;
+  border-bottom: 1px solid ${(props) => props.color};
 `;
 
 const VLine = styled.div`
   width: 1px;
   height: ${(props) => (props.size === null ? '100%' : `${props.size}px`)};
-  border-left: 1px solid #cecece;
+  border-left: 1px solid ${(props) => props.color};
 `;
 
-const Divider = ({ vertical, size }) => {
-  if (vertical) return <VLine size={size} />;
-  return <HLine size={size} />;
+const Divider = ({ vertical, size, color }) => {
+  if (vertical) return <VLine size={size} color={color} />;
+  return <HLine size={size} color={color} />;
 };
 
 Divider.propTypes = {
   vertical: PropTypes.bool,
   size: PropTypes.number,
+  color: PropTypes.string,
 };
 
 Divider.defaultProps = {
   vertical: false,
   size: null,
+  color: '#cecece',
 };
 
 export default Divider;
