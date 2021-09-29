c99240b8aa10902f26862495f000fffb8bd89e5f
devHudi <devhudi@gmail.com>
2021-09-30 08:01:57 +0900

    feat: set height

--- src/moby-ui/Button/index.jsx
+++ src/moby-ui/Button/index.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export default styled.button`
   padding: 8px;
   width: ${(props) => `${props.width}px` || '100%'};
+  height: 32px;
   background-color: #512cdf;
   color: #ffffff;
   font-size: 12px;
