838203944ba36a1436695c490d6790301123ecd3
devHudi <devhudi@gmail.com>
2021-10-01 19:25:36 +0900

    feat: add color prop

--- src/moby-ui/Typography/index.jsx
+++ src/moby-ui/Typography/index.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 const Typography = styled.div`
+  color: ${(props) => props.color || '#000000'};
   font-size: ${(props) => `${props.size}px` || '16px'};
   font-weight: ${(props) => props.weight || 'normal'};
   text-align: ${(props) => props.align || 'left'};
