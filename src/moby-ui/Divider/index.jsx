3237b1d15be21b71bd40c9c92b00e7e92615c383
devHudi <devhudi@gmail.com>
2021-10-01 19:25:58 +0900

    feat: add weight props

--- src/moby-ui/Divider/index.jsx
+++ src/moby-ui/Divider/index.jsx
@@ -3,30 +3,32 @@ import styled from 'styled-components';
 
 const HLine = styled.div`
   width: ${(props) => (props.size === null ? '100%' : `${props.size}px`)};
-  border-bottom: 1px solid ${(props) => props.color};
+  border-bottom: ${(props) => props.weight}px solid ${(props) => props.color};
 `;
 
 const VLine = styled.div`
   width: 1px;
   height: ${(props) => (props.size === null ? '100%' : `${props.size}px`)};
-  border-left: 1px solid ${(props) => props.color};
+  border-left: ${(props) => props.weight}px solid ${(props) => props.color};
 `;
 
-const Divider = ({ vertical, size, color }) => {
-  if (vertical) return <VLine size={size} color={color} />;
-  return <HLine size={size} color={color} />;
+const Divider = ({ vertical, size, color, weight }) => {
+  if (vertical) return <VLine size={size} color={color} weight={weight} />;
+  return <HLine size={size} color={color} weight={weight} />;
 };
 
 Divider.propTypes = {
   vertical: PropTypes.bool,
   size: PropTypes.number,
   color: PropTypes.string,
+  weight: PropTypes.number,
 };
 
 Divider.defaultProps = {
   vertical: false,
   size: null,
   color: '#cecece',
+  weight: 1,
 };
 
 export default Divider;
