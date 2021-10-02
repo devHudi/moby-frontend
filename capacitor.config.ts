f309a445547d84b1e92f60e950682689369b6129
devHudi <devhudi@gmail.com>
2021-10-02 20:37:22 +0900

    chore: set capacitor for build app

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.current.moby',
+  appName: 'moby-frontend',
+  webDir: 'build',
+  bundledWebRuntime: false,
+};
+
+export default config;
